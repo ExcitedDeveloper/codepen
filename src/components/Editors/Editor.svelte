3e8864e37204a3485f9afd687a4d620d09c04684
Steve Hunley <shunley2000@yahoo.com>
2023-10-29 19:07:31 -0500

    Added tabs

--- src/components/Editors/Editor.svelte
+++ src/components/Editors/Editor.svelte
@@ -9,12 +9,20 @@
   export let classes;
 </script>
 
-<div {id} class={`editor ${classes}`}></div>
+<div {id} class={`editor ${classes}`}>
+  <div class="editor-powers">
+    <div class="editor-title"></div>
+    <div class="editor-actions"></div>
+  </div>
+  <div class="editor-code"></div>
+</div>
 
 <style>
   .editor {
     border: 1px solid teal;
-    background: rgb(29, 30, 34);
+    --editor-action-height: 36px;
+    --editor-background: rgb(29, 30, 34);
+    background: var(--editor-background);
   }
 
   .html-editor {
@@ -27,5 +35,24 @@
 
   .js-editor {
     grid-area: jseditor;
+  }
+
+  .editor-powers {
+    height: var(--editor-action-height);
+    display: flex;
+  }
+
+  .editor-title {
+    background: var(--editor-background);
+    width: 96px;
+  }
+
+  .editor-actions {
+    background: var(--app-background);
+    flex: 1;
+  }
+
+  .editor-code {
+    height: calc(100% - var(--editor-action-height));
   }
 </style>
