e742aeb3837ff40e1c8dc97e6c5593bfa4407cdc
Steve Hunley <shunley2000@yahoo.com>
2023-11-14 19:41:41 -0600

    css and js collapsing

--- src/utils/expandCollapse.ts
+++ src/utils/expandCollapse.ts
@@ -33,7 +33,7 @@ export const collapseHTMLEditor = (
       cssEditor?.getBoundingClientRect().width +
       jsEditor?.getBoundingClientRect().width / 2;
     jsEditorColWidth =
-      content.clientWidth - dragbarWidth * 3 - htmlEditorColWidth;
+      content.clientWidth - dragbarWidth * 3 - cssEditorColWidth;
   } else if (cssEditorIsCollapsed) {
     // The css editor is collapsed.  Give the js editor all of the
     // available space.
@@ -63,16 +63,40 @@ export const collapseHTMLEditor = (
 export const collapseCSSEditor = (
   content: NullableHTMLElement,
   htmlEditor: NullableHTMLElement,
-  cssEditor: NullableHTMLElement,
+  jsEditor: NullableHTMLElement,
 ) => {
-  if (!content || !htmlEditor || !cssEditor) return;
+  if (!content || !htmlEditor || !jsEditor) return;
 
-  const htmlEditorColWidth =
-    htmlEditor?.getBoundingClientRect().width +
-    cssEditor?.getBoundingClientRect().width / 2;
+  const htmlEditorIsCollapsed = get(HTMLEditorOpenStore) === OpenState.Closed;
+  const jsEditorIsCollapsed = get(JSEditorOpenStore) === OpenState.Closed;
+
+  // If html and js editors are collapses, then don't
+  // collapse css editor
+  if (htmlEditorIsCollapsed && jsEditorIsCollapsed) return;
+
   const cssEditorColWidth = 0;
-  const jsEditorColWidth =
-    content?.clientWidth - dragbarWidth * 3 - htmlEditorColWidth;
+  let htmlEditorColWidth;
+  let jsEditorColWidth;
+
+  if (!htmlEditorIsCollapsed && !jsEditorIsCollapsed) {
+    // Neither the html or the js editor are collapsed.
+    // Give each editor half of the available space.
+    htmlEditorColWidth =
+      htmlEditor?.getBoundingClientRect().width +
+      jsEditor?.getBoundingClientRect().width / 2;
+    jsEditorColWidth =
+      content.clientWidth - dragbarWidth * 3 - htmlEditorColWidth;
+  } else if (htmlEditorIsCollapsed) {
+    // The html editor is collapsed.  Give the js editor all of the
+    // available space.
+    htmlEditorColWidth = 0;
+    jsEditorColWidth = content.clientWidth - dragbarWidth * 3;
+  } else {
+    // The js editor is collapsed.  Give the html editor all of the
+    // available space.
+    htmlEditorColWidth = content.clientWidth - dragbarWidth * 3;
+    jsEditorColWidth = 0;
+  }
 
   let cols = [
     dragbarWidth,
@@ -91,16 +115,40 @@ export const collapseCSSEditor = (
 export const collapseJSEditor = (
   content: NullableHTMLElement,
   htmlEditor: NullableHTMLElement,
-  jsEditor: NullableHTMLElement,
+  cssEditor: NullableHTMLElement,
 ) => {
-  if (!content || !htmlEditor || !jsEditor) return;
+  if (!content || !cssEditor || !htmlEditor) return;
 
-  const htmlEditorColWidth =
-    htmlEditor?.getBoundingClientRect().width +
-    jsEditor?.getBoundingClientRect().width / 2;
-  const cssEditorColWidth =
-    content?.clientWidth - dragbarWidth * 3 - htmlEditorColWidth;
+  const cssEditorIsCollapsed = get(CSSEditorOpenStore) === OpenState.Closed;
+  const htmlEditorIsCollapsed = get(HTMLEditorOpenStore) === OpenState.Closed;
+
+  // If css and html editors are collapses, then don't
+  // collapse js editor
+  if (cssEditorIsCollapsed && htmlEditorIsCollapsed) return;
+
   const jsEditorColWidth = 0;
+  let cssEditorColWidth;
+  let htmlEditorColWidth;
+
+  if (!cssEditorIsCollapsed && !htmlEditorIsCollapsed) {
+    // Neither the css or the html editor are collapsed.
+    // Give each editor half of the available space.
+    cssEditorColWidth =
+      cssEditor?.getBoundingClientRect().width +
+      htmlEditor?.getBoundingClientRect().width / 2;
+    htmlEditorColWidth =
+      content.clientWidth - dragbarWidth * 3 - cssEditorColWidth;
+  } else if (cssEditorIsCollapsed) {
+    // The css editor is collapsed.  Give the html editor all of the
+    // available space.
+    cssEditorColWidth = 0;
+    htmlEditorColWidth = content.clientWidth - dragbarWidth * 3;
+  } else {
+    // The html editor is collapsed.  Give the css editor all of the
+    // available space.
+    cssEditorColWidth = content.clientWidth - dragbarWidth * 3;
+    htmlEditorColWidth = 0;
+  }
 
   let cols = [
     dragbarWidth,
